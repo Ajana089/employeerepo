49d366e39872a77002cc22b5aa537388e74ef3f2
Ajana Antony <ajanaantony089@gmail.com>
2025-03-07 16:33:14 +0400

    pg

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+
+export const routes: Routes = [
+    {path:'',component:LoginpageComponent},
+    {path:'login',component:LoginpageComponent}
+
+];
