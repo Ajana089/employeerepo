2b6473d2635cb728c17dfdf5b482a700103cec14
Ajana Antony <ajanaantony089@gmail.com>
2025-03-10 13:30:47 +0400

    try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,35 @@
 import { Routes } from '@angular/router';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeregistrationComponent } from './pages/employeeregistration/employeeregistration.component';
+import { LeaveComponent } from './pages/leave/leave.component';
 
 export const routes: Routes = [
-    {path:'',component:LoginpageComponent},
-    {path:'login',component:LoginpageComponent}
+    {path:'',
+     redirectTo:'login',
+     pathMatch:'full'
+
+    },
+    
+    {path:'login',component:LoginpageComponent},
+    {
+        path:'',
+        component:LayoutComponent,
+        children:
+        [{
+            path:'dashboard',
+            component:DashboardComponent
+        },
+    {
+        path:'employe',
+        component:EmployeeregistrationComponent,
+
+    },
+{
+    path:'leave',
+    component:LeaveComponent
+}]
+    }
 
 ];
